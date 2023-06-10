d72c4c33b4a0e623e0e51c930594b761629599e3
Akshay Jadhav <akshayjadhav461999@gmail.com>
2023-06-10 19:00:05 +0530

    Displaying data in a list

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,25 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Akshay Anil Jadhav on 10/06/23.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var scrums = DailyScrum.sampleData
+    static var previews: some View {
+        ScrumsView(scrums: scrums)
+    }
+}
