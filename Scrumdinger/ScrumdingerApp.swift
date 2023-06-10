cb14aa76144984064eb6181657e306a6153e7c56
Akshay Jadhav <akshayjadhav461999@gmail.com>
2023-06-10 15:24:41 +0530

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Akshay Anil Jadhav on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
